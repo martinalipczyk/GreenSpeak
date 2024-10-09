3155fad104802dc31ee4a2cc529a05976a3add52
liamstrauser <liamstrauser@gmail.com>
2024-10-09 19:12:49 -0400

    reinitializing express and server

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require( "express" );
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname + "/public"));
+app.set( "views",  __dirname + "/views");
+
+app.get( "/", ( req, res ) => {
+    res.sendFile( __dirname + "/views/index.html" );
+} );
+
+app.listen( port, () => {
+    console.log(`App server listening on ${ port }. (Go to http://localhost:${ port })` );
+} );
