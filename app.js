3880bb2752c02c40d38257469179da58039621ce
martinalipczyk <marlip26@bergen.org>
2024-10-10 21:35:04 -0400

    idk

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(express.static(__dirname + "/public"));
 app.set( "views",  __dirname + "/views");
 
 app.get( "/", ( req, res ) => {
-    res.sendFile( __dirname + "/views/index.html" );
+    res.sendFile( __dirname + "/views/home.html" );
 } );
 
 app.listen( port, () => {
