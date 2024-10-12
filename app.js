0750063716151d3f7e077a6eea06a615ba7861eb
liamstrauser <liamstrauser@gmail.com>
2024-10-12 17:59:14 -0400

    opportunity submission + database initialization

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const express = require( "express" );
+const express = require("express");
 const app = express();
 const port = 3000;
 const logger = require("morgan");
+const path = require("path");
+const db = require("./db/db_connection");
 
 app.use(logger("dev"));
 app.use(express.static(__dirname + "/public"));
 app.set( "views",  __dirname + "/views");
+app.use(express.urlencoded({ extended: true }));
 
 app.get( "/", ( req, res ) => {
     res.sendFile( __dirname + "/views/home.html" );
