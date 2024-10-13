ca6d21323929180d24645565eb77eb8187ecfd2e
liamstrauser <liamstrauser@gmail.com>
2024-10-12 20:36:46 -0400

    article submission function working

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ app.get( "/", ( req, res ) => {
     res.sendFile( __dirname + "/views/home.html" );
 } );
 
+app.get( "/articles", ( req, res ) => {
+    res.sendFile( __dirname + "/views/articles.html" );
+} );
+
+app.get( "/articlesub", ( req, res ) => {
+    res.sendFile( __dirname + "/views/articlesub.html" );
+} );
+
 app.get( "/opps", ( req, res ) => {
     res.sendFile( __dirname + "/views/opps.html" );
 } );
@@ -33,6 +41,20 @@ VALUES (?, ?, ?, ?, ?, ?, ?)`;
             return res.status(500).send("There was an error submitting your event.");
         }
         res.send("Event submitted successfully!");
+    });
+});
+
+app.post("/submit-article", (req, res) => {
+    console.log(req.body);
+    const { first_name, last_name, title, text, date, email, institution } = req.body;
+    const asub = `INSERT INTO articles (first_name, last_name, title, text, date, email, institution) 
+VALUES (?, ?, ?, ?, ?, ?, ?)`;
+
+    db.execute(asub, [first_name, last_name, title, text, date, email, institution], (error, result) => {
+        if (error) {
+            return res.status(500).send("There was an error submitting your article.");
+        }
+        res.send("article");
     });
 });
 
